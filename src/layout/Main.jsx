20829984e7c8b1b1d39c3871231f395f7f6443ac
Vsevolod Hella <xlisentis@gmail.com>
2022-06-24 13:23:21 +0300

    Search and filter functionality added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,30 +1,43 @@
-import React from "react"
-import { MovieList } from "../components/MovieList"
-import { Preloader } from "../components/Preloader"
+import React from 'react';
+import { MovieList } from '../components/MovieList';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 class Main extends React.Component {
-
     state = {
         movies: [],
-    }
+    };
 
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=39f9675a&s=matrix&page=2')
-            .then(res => res.json())
-            .then(data => this.setState({movies: data.Search}))
+        fetch('http://www.omdbapi.com/?apikey=39f9675a&s=matrix')
+            .then((res) => res.json())
+            .then((data) => this.setState({ movies: data.Search }));
     }
-    
-    render() {
-        const{movies} = this.state;
 
-         return <main className="content container">
-            {
-                movies.length ? (
-                <MovieList movies={this.state.movies} />
-                ) : <Preloader />
-            }
-        </main>
-    } 
+    searchMovies = (str, type = 'all') => {
+        fetch(
+            `http://www.omdbapi.com/?apikey=39f9675a&s=${str}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`
+        )
+            .then((res) => res.json())
+            .then((data) => this.setState({ movies: data.Search }));
+    };
+
+    render() {
+        const { movies } = this.state;
+
+        return (
+            <main className='content container'>
+                <Search searchMovies={this.searchMovies} />
+                {movies.length ? (
+                    <MovieList movies={this.state.movies} />
+                ) : (
+                    <Preloader />
+                )}
+            </main>
+        );
+    }
 }
 
-export {Main}
+export { Main };
